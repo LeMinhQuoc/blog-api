6b5a42cd7bdded1152ac50c07f96dd3c427be29f
minh-tong <minh.tong@sunbytes.vn>
2021-08-11 15:37:27 +0700

    add userid to token

--- src/EventListener/JWTCreatedListener.php
+++ src/EventListener/JWTCreatedListener.php
@@ -1,32 +1,36 @@
 <?php
+namespace App\EventListener;
+
+use App\Repository\UserRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 use Symfony\Component\HttpFoundation\RequestStack;
 
+
 class JWTCreatedListener
 {
-/**
- * @var RequestStack
- */
-private $requestStack;
 
-/**
- * @param RequestStack $requestStack
- */
-public function __construct(RequestStack $requestStack)
-{
-    $this->requestStack = $requestStack;
-}
+    protected $userRepository;
 
-/**
- * @param JWTCreatedEvent $event
- *
- * @return void
- */
-public function onJWTCreated(JWTCreatedEvent $event)
-{
-    $request = $this->requestStack->getCurrentRequest();
-    $payload       = $event->getData();
-    $user = $this->userRepository->findOneBy(['username' => $payload['username'] ]);
-    $payload['userId'] = $user->getId();
-}
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct( UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+
+    /**
+     * @param JWTDecodedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $payload  = $event->getData();
+        $user = $this->userRepository->findOneBy(['username' => $payload['username']]);
+        $payload['userId'] =  $user->getId();
+
+        $event->setData($payload);
+    }
 }
